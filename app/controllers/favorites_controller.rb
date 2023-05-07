92c88321ecae750e0f3137ac486089e049a1115d
odendayoko <128268777+odendayoko@users.noreply.github.com>
2023-05-07 11:22:21 +0900

    お気に入りをしたとき、そのページにリダイレクトするようにした

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.build(menu_id: params[:menu_id])
     favorite.save
-    redirect_to root_path
+    redirect_to request.referer
   end
 
   def destroy
     favorite = Favorite.find_by(menu_id: params[:menu_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to root_path
+    redirect_to request.referer
   end
 end
