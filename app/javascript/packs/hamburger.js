920e7a991ea8190a00601de36c2028d122cb56c3
odendayoko <128268777+odendayoko@users.noreply.github.com>
2023-05-10 00:40:16 +0900

    詳細ページをログインしていなくてもみられるようにした、ハンバーガーメニューを実装した、編集画面で保存を押すと削除されてしまう仕様を修正した

---
+++ app/javascript/packs/hamburger.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.hamburger-menu');
+
+  hamburger.addEventListener('click', () => {
+    menu.classList.toggle('show');
+});
+
+}
