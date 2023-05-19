ddd0b3f75b6b99dafb1035e256d31a5860b27c85
odendayoko <128268777+odendayoko@users.noreply.github.com>
2023-05-19 16:19:07 +0900

    s3の設定変更、OGPの変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,14 +5,10 @@ module ApplicationHelper
       title: 'レシピの記録サイト',
       reverse: true,
       charset: 'utf-8',
-      description: 'Recipistを使えば自分が作った料理のレシピを、レシピサイトのURLと一緒に記録し、シェアすることができます。',
+      description: '日々の料理レシピを、レシピサイトのURLと一緒に記録し、シェアできます。',
       keywords: '料理,レシピ,ご飯',
       canonical: request.original_url,
       separator: '|',
-      # icon: [
-      #   { href: image_url('ogp.png') },
-      #   { href: image_url('top_image.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
-      # ],
       og: {
         site_name: :site,
         title: :title,
@@ -24,7 +20,6 @@ module ApplicationHelper
       },
       twitter: {
         card: 'summary_large_image',
-        site: '@odendayoko',
         image: nil,
       }
     }
