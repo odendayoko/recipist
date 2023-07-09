06030b81622f8cce4607c7b709ec340a866c8a16
odendayoko <128268777+odendayoko@users.noreply.github.com>
2023-07-09 09:41:24 +0900

    Menuモデルの単体テスト実装

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -2,20 +2,38 @@ require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
   before do
-    @user = User.create(nickname: 'テストユーザー')
     @menu = FactoryBot.build(:menu)
   end
-  describe 'レシピ投稿機能' do
-    it 'nameが空では登録できない' do    
-      @menu.name = ''
-      @menu.valid?
-      expect(@menu.errors.full_messages).to include("レシピ名を入力してください")
+  describe 'レシピ投稿' do
+    context 'レシピが投稿できる場合' do
+      it '料理名、URL、メモ、写真を投稿できる' do
+        expect(@menu).to be_valid
+      end
+      it 'メモがなくても投稿できる' do
+        @menu.memo = ''
+        expect(@menu).to be_valid
+      end
+      it '写真がなくても投稿できる' do
+        @menu.image = nil
+        expect(@menu).to be_valid
+      end
     end
-    it 'urlが空では登録できない' do
-      @menu.url = ''
-      @menu.valid?
-      binding.pry
-      expect(@menu.errors.full_messages).to include("URLを入力してください")
+    context 'レシピが投稿できない場合' do
+      it '料理名が空では登録できない' do    
+        @menu.name = ''
+        @menu.valid?
+        expect(@menu.errors.full_messages).to include("レシピ名を入力してください")
+      end
+      it 'URLが空では登録できない' do
+        @menu.url = ''
+        @menu.valid?
+        expect(@menu.errors.full_messages).to include("URLを入力してください")
+      end
+      it 'ユーザーが紐づいていなければ投稿できない' do
+        @menu.user = nil
+        @menu.valid?
+        expect(@menu.errors.full_messages).to include("Userを入力してください")
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
     end
-    context '新規登録dけいないとき' do
+    context '新規登録できないないとき' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
